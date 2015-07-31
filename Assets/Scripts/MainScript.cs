3804a086f9144e2b039b68282c143ece96ed3fe2
seaders <seaders69@gmail.com>
2015-07-31 15:06:55 +0100

    Initial commit.

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MainScript : MonoBehaviour
+{
+	void Start ()
+	{
+		Action a = () => Debug.Log("Nothing to see here");
+
+        ThisTakesACallback(a);
+
+        Action b;
+
+        b = () => {
+            Debug.Log(b.Method.Name);
+        };
+
+        b.Invoke();
+	}
+
+	void ThisTakesACallback(Action a)
+	{
+        ThisTakesAForwardedCallback(a ?? ThisIsWrongButItCompiles);
+    }
+
+    void ThisTakesAForwardedCallback(Action a)
+    {
+        Debug.Log("callback: " + a.Method.Name);
+        a.Invoke();
+    }
+
+	void ThisIsWrongButItCompiles(bool wrong=false)
+	{
+		Debug.Log("wrong: " + wrong);
+	}
+}
